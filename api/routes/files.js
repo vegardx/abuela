4b6418b188091b11fdde513dcec07be029a07693
Anders Heivoll <anders@savant.no>
2020-03-01 15:40:57 +0100

    Setting up to serve everything through Express.

--- api/routes/files.js
+++ api/routes/files.js
@@ -8,4 +8,15 @@ router.get('/photos/:photo', (req, res) => {
     res.sendFile(path.join(`${baseroot}/photos/${req.params.photo}`));
 });
 
+router.get("/*", (req, res) => {
+    let url = req.url.replace(/^\/c\//g, '/');
+
+    if (url.match(/\./)) {
+        res.sendFile(path.join(baseroot + "/client/build/" + url));
+    }
+    else {
+        res.sendFile(path.join(baseroot + "/client/build/index.html"));
+    }
+});
+
 module.exports = router;
